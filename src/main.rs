cf865f36a77a9d3e844c263c9d4e18c13cd09fc7
kper <kevp.per@gmail.com>
2016-11-29 08:32:58 +0000

    implement sync methods

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,23 @@
 extern crate rustc_serialize;
 extern crate uuid;
 extern crate bincode;
-
 extern crate iron;
 extern crate router;
 extern crate params;
-
-extern crate byteorder;
-
 extern crate chrono;
+extern crate base64;
 
 use iron::status;
 use router::Router;
 use iron::prelude::*;
-use params::Params;
+use params::{Params, Value};
 
 use rustc_serialize::json::{self, Json};
 use bincode::rustc_serialize::{encode_into, decode_from, EncodingError, DecodingError};
 use bincode::SizeLimit;
 
-use std::fs::{File, read_dir, remove_file};
+use std::fs::{File, read_dir, remove_file, Metadata, metadata};
+use std::time::{SystemTime, UNIX_EPOCH};
 
 use uuid::Uuid;
 
@@ -32,6 +30,9 @@ use std::io::Error as IoError;
 use std::net::{SocketAddrV4, Ipv4Addr};
 
 use chrono::*;
+use base64::{encode, decode};
+
+use std::rc::Rc;
 
 fn http_all_get(req: &mut Request) -> IronResult<Response> {
     let files = RcFile::get_all();
@@ -64,15 +65,19 @@ fn http_post(req: &mut Request) -> IronResult<Response> {
 
     let doc: RcFile = json::decode(&js.to_string()).unwrap();
 
+    let mut f = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .open(format!("./data/{}", doc.file_id))
+        .unwrap();
+
+    encode_into(&doc, &mut f, SizeLimit::Infinite);
+
     Ok(Response::with((status::Ok, json::encode(&doc).unwrap())))
 }
 
-fn http_sync_get(req: &mut Request) -> IronResult<Response> {
-    unimplemented!()
-}
-fn http_sync_post(req: &mut Request) -> IronResult<Response> {
-    unimplemented!()
-}
+
+
 fn http_delete(req: &mut Request) -> IronResult<Response> {
     let ref file_id = req.extensions
         .get::<Router>()
@@ -88,12 +93,89 @@ fn http_delete(req: &mut Request) -> IronResult<Response> {
     Ok(res)
 }
 
+// Update on server
+fn http_push(req: &mut Request) -> IronResult<Response> {
+    let map = req.get_ref::<Params>().unwrap();
+
+    let js: Json = json::Json::from_str(&format!("{:?}", map)).unwrap();
+
+    let doc: Vec<(Uuid, DateTime<UTC>)> = json::decode(&js.to_string()).unwrap();
+
+    let mut requesting: Vec<Uuid> = Vec::new();
+
+    let files = RcFile::get_all();
+
+    for (id, time) in doc {
+        if (false) {
+            requesting.push(id);
+        }
+    }
+
+    Ok(Response::with((status::Ok, json::encode(&requesting).unwrap())))
+}
+
+// Update on client
+fn http_pull(req: &mut Request) -> IronResult<Response> {
+    let files = RcFile::get_all();
+    let mut result: Vec<(Uuid, DateTime<UTC>)> = Vec::new();
+
+    for f in files {
+        let attr = metadata(f.filename).unwrap();
+        let time = attr.modified().unwrap();
+
+        result.push((f.file_id, system_time_to_date_time(time)));
+    }
+
+    Ok(Response::with((status::Ok, json::encode(&result).unwrap())))
+}
+
+fn system_time_to_date_time(t: SystemTime) -> DateTime<UTC> {
+    let (sec, nsec) = match t.duration_since(UNIX_EPOCH) {
+        Ok(dur) => (dur.as_secs() as i64, dur.subsec_nanos()),
+        Err(e) => {
+            // unlikely but should be handled
+            let dur = e.duration();
+            let (sec, nsec) = (dur.as_secs() as i64, dur.subsec_nanos());
+            if nsec == 0 {
+                (-sec, 0)
+            } else {
+                (-sec - 1, 1_000_000_000 - nsec)
+            }
+        }
+    };
+    UTC.timestamp(sec, nsec)
+}
+
+fn http_sync_file(req: &mut Request) -> IronResult<Response> {
+    let map = req.get_ref::<Params>().unwrap();
+
+    match map.find(&["payload"]) {
+        Some(&Value::String(ref payload)) => {
+            match map.find(&["fileid"]) {
+                Some(&Value::String(ref file_id)) => {
+                    let mut file = RcFile::get(Uuid::parse_str(file_id).unwrap()).unwrap();
+                    file.payload = payload.to_string();
+
+                    let mut file_handler = File::open(&file.filename).unwrap();
+
+                    encode_into(&file, &mut file_handler, SizeLimit::Infinite);
+
+                    Ok(Response::with(status::Ok))
+                } 
+                _ => Ok(Response::with(iron::status::NotFound)),
+            }
+        }
+        _ => Ok(Response::with(iron::status::NotFound)),
+    }
+}
+
 fn main() {
     let mut router = Router::new();
     router.get("/files", http_all_get, "get_files");
     router.get("/files/:file_id", http_get, "get_file");
-    router.get("/file/sync", http_sync_get, "http_sync_get");
-    router.post("/file/sync", http_sync_post, "http_sync_post");
+    router.post("/file/push", http_push, "http_push");
+    router.post("/file/pull", http_pull, "http_pull");
+    router.post("/file/sync", http_sync_file, "http_sync_file");
     router.post("/file", http_post, "post_file");
     router.delete("/files/:file_id", http_delete, "delete_file");
 
@@ -120,12 +202,17 @@ impl RcFile {
         }
     }
 
-    fn get_all() -> Vec<String> {
-        let mut files: Vec<String> = Vec::new();
+    fn get_all() -> Vec<RcFile> {
+        let mut files: Vec<RcFile> = Vec::new();
         let paths = read_dir("./data").unwrap();
 
         for path in paths {
-            let file: String = path.unwrap().path().to_str().unwrap().to_string();
+            let file_name: String = path.unwrap().path().to_str().unwrap().to_string();
+
+            let mut filehandler = File::open(file_name).unwrap();
+
+            let file = decode_from(&mut filehandler, SizeLimit::Infinite).unwrap();
+
             files.push(file);
         }
 
